4e7d752bd7dfe3300d2022fd2930260b7662bdb8
husnamahbub <husna9876ara@gmail.com>
2023-11-22 18:42:07 +0600

    database theke call kore job er details page ar kaj seshs

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
@@ -26,6 +26,8 @@ async function run() {
 
   }
 }
+
+
 run().catch(console.dir);
 
 const addJobsCollections = client.db('AllCategoryOfJobs').collection('clientsAddedJobs');
@@ -43,9 +45,24 @@ app.post('/addJobs', async(req, res) => {
   const result = await addJobsCollections.insertOne(cosmetic);
   res.send(result)
 })
+app.get('/categoryJobs', async(req, res) => {
+  const cursor = addJobsCollections.find()
+  const result = await cursor.toArray();
+  res.send(result)
+})
+
+app.get('/jobDetails/:id', async(req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id)};
+  const result = await addJobsCollections.findOne(query);
+  res.send(result);
+})
+
+
 app.get('/', (req, res) => {
    res.send("welcome to my server side")
 })
+
 
 app.listen(port, () => {
     console.log(`Welcome  to our server side ${port}`);
